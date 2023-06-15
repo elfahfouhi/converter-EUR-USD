74353ca834027045623b9d7e0bc5d451af922e42
redouane <redouane.elfahfouhi@gmail.com>
2023-06-15 09:46:56 +0200

    WIP : delete no usable component and some refacto

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -1,7 +1,21 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import { BehaviorSubject, combineLatest, Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { SubSink } from 'subsink';
 import { ExchangeRateStoreService } from './services/exchange-rate-store.service';
+const rotate: { [key: string]: string } = { 'EUR': 'USD', 'USD': 'EUR' };
+
+export interface ConverterHisto{
+  realExchangeRate?:number;
+  fixedExchangeRate?:number;
+  inputAmount?:Amount;
+  outputAmount?:Amount;
+}
+export interface Amount{
+  value:number;
+  currency:string;
+}
 
 @Component({
   selector: 'app-converter',
@@ -9,19 +23,86 @@ import { ExchangeRateStoreService } from './services/exchange-rate-store.service
   styleUrls: ['./converter.component.scss']
 })
 export class ConverterComponent implements OnInit,OnDestroy {
-  interval: any;
+  outputCurrency = "USD";
+  inputCurrency = "EUR";
 
-  constructor(public store:ExchangeRateStoreService) { }
-   fixedExchangeRate: number=1.1;
-   isChedked:boolean=true;
-  ngOnInit(): void {
-    
+  inputAmountTerm$ = new BehaviorSubject<number>(0);
+  inputCurrencyTerm$ = new BehaviorSubject<string>("EUR");
+
+  fixedExchangeRate$ = new BehaviorSubject<number>(0);
+  outputAmountTerm$: Observable<number> = of(0);
+
+  inputAmountTermControl = new FormControl();
+  fixedExchangeRateControl = new FormControl();
+
+  isChekedFixedExchangeRate: boolean = false;
+  usedExchangeRate: string = '';
+  subs = new SubSink();
+  listOperationHisto:ConverterHisto[]=[];
+  constructor(public store: ExchangeRateStoreService) {
+
+    this.outputAmountTerm$ = combineLatest([this.store.exhangeRate$,  
+                                            this.inputAmountTerm$,
+                                            this.inputCurrencyTerm$, 
+                                            this.fixedExchangeRate$]).
+                                            pipe(
+                                            map(([exchangeRate,
+                                                  inputAmountTerm,
+                                                  inputCurrencyTerm,
+                                                  fixedExchangeRate]) => {
+
+                                                    let outputAmountTerm;
+                                              if ((Math.abs((exchangeRate - fixedExchangeRate)) / fixedExchangeRate) > 0.02 && this.isChekedFixedExchangeRate) {
+                                                this.usedExchangeRate = 'fixé'
+                                                outputAmountTerm=inputCurrencyTerm == "EUR" ? inputAmountTerm * fixedExchangeRate : inputAmountTerm / fixedExchangeRate
+                                                //TODO Factorisé pour ne pas appler deux foix saveOperation
+                                                this.saveOperation({value:inputAmountTerm,currency:inputCurrencyTerm},{value:outputAmountTerm,currency:this.outputCurrency},exchangeRate,fixedExchangeRate)
+
+                                                return outputAmountTerm
+                                              }
+                                              this.usedExchangeRate = 'réel'
+                                              outputAmountTerm=inputCurrencyTerm == "EUR" ? inputAmountTerm * exchangeRate : inputAmountTerm / exchangeRate 
+                                                                                            
+                                                //TODO Factorisé pour ne pas appler deux foix saveOperation                                               
+                                                this.saveOperation({value:inputAmountTerm,currency:inputCurrencyTerm},{value:outputAmountTerm,currency:this.outputCurrency},exchangeRate,fixedExchangeRate)
+
+                                              return outputAmountTerm;
+                                            })
+                                             
+                                          )
   }
-  
-  subs=new SubSink();
 
-ngOnDestroy(){
-  this.subs.unsubscribe();
-}
+  ngOnInit(): void {
+    //value change sur le montant en input pour declencher l'event dans le combinelatest
+    this.subs.sink = this.inputAmountTermControl.valueChanges.subscribe((value) => {
+      this.inputAmountTerm$.next(value);
+    }
+    )
+    //value change sur le taux fixé pour declencher l'event dans le combinelatest
+    this.fixedExchangeRateControl.valueChanges.subscribe((value) => {
+      this.fixedExchangeRate$.next(value);
+    }
+    )
+
+  }
+  //permet de switcher entre EUR et USD
+  chooseCurrency($event: any) {
+    this.inputCurrencyTerm$.next($event);
+    this.outputCurrency = rotate[$event]
+  }
+  //TODO remove to service
+  saveOperation(inputAmount:Amount,outputAmount:Amount,realExchangeRate:number,fixedExchangeRate:number){
+    this.listOperationHisto.push({realExchangeRate:realExchangeRate,
+                                 fixedExchangeRate:fixedExchangeRate,
+                                 inputAmount:inputAmount,
+                                 outputAmount:outputAmount});
+    //extraire les 5 derniers element de la liste                              
+    this.listOperationHisto = this.listOperationHisto.slice(Math.max(this.listOperationHisto.length - 5, 0));                            
+  }
+
+  ngOnDestroy() {
+    //désinscrire de tout les souscribtions
+    this.subs.unsubscribe();
+  }
 
 }
--- src/app/converter/converter.module.ts
+++ src/app/converter/converter.module.ts
@@ -1,29 +1,18 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ConverterRoutingModule } from './converter-routing.module';
 import { ConverterComponent } from './converter.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CurrencyComponent } from './currency/currency.component';
-import { UiSwitchModule } from 'ngx-toggle-switch';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+
 
 
 @NgModule({
-  declarations: [ConverterComponent, CurrencyComponent],
+  declarations: [ConverterComponent],
   imports: [
     CommonModule,
     ConverterRoutingModule,
     FormsModule,
-    ReactiveFormsModule,
-    NgbModule,
-    UiSwitchModule,
-    MatButtonToggleModule,
-    MatFormFieldModule,
-    MatInputModule
-  ]})
+    ReactiveFormsModule
+  ]
+})
 export class ConverterModule { }
